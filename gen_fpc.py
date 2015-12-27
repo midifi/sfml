ac1ba76b352830b98942b1d953db275e739d2012
Ryan Gonzalez <rymg19@gmail.com>
2015-12-27 15:26:09 -0600

    Rewrite gen_fpc in Python

---
+++ gen_fpc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess, os
+
+packages = ['system', 'window', 'graphics']
+
+template = '''
+Name: sfml-{pkg}
+Description: Felix bindings for SFML (Simple Fast Multimedia Library) {pkg}.
+provides_dlib: {libs}
+provides_slib: {libs}
+'''.strip()
+
+basedir = os.path.dirname(__file__)
+
+def write(pkg, libs):
+    with open(os.path.join(basedir, 'sfml-fpc', 'sfml-%s.fpc' % pkg), 'w') as f:
+        f.write(template.format(pkg=pkg, libs=libs) + '\n')
+
+if __name__ == '__main__':
+    for pkg in packages:
+        libs = subprocess.check_output(['pkg-config', '--libs', 'sfml-' + pkg]).\
+              decode('ascii').strip()
+        write(pkg, libs)
